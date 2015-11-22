b0159f28ddf9ee35df762445e6f439f801b06b6e
tzw0745 <1976850082@qq.com>
2015-11-22 11:04:38 +0800

    包含函数结果状态代码和Status的定义
    p10 1.3 抽象数据类型的表示及实现

---
+++ Tree/define.h
@@ -0,0 +1,17 @@
+#ifndef DEFINE_H
+#define DEFINE_H
+
+//Status是函数的类型 其值是函数结果状态代码
+typedef int Status;
+
+//函数结果状态代码
+#define TRUE            1
+#define FALSE           0
+
+#define OK              1
+#define ERROR           0
+
+#define INFEASIBLE      -1
+#define OVERFLOW        -2
+
+#endif
